db7ecc16d3d7b020b38623fd7f491bcaf236d4ba
Rodrigo Rubio Haro <chavo0022009@gmail.com>
2020-03-02 15:48:42 -0600

    ESCOM 3.2 Pruebas2 was added

---
+++ escom/De la O/Departamental1/Practicas/Rubio_Haro_Rodrigo_Rodolfo_Practica03_2CV1_Departamental1/CodigoFuente/2.Prueba2/Prueba2.java
@@ -0,0 +1,9 @@
+// Se agrego el public a la clase prueba2 y a esta se le cambio el nombre a Prueba2
+public class Prueba2 {
+    // Se corrigio string por String
+    public static void main(String args[]) {
+        // Se corrigo system por System y se le agego un punto faltante
+        System.out.println("\u0007 \u0007 Hola Mundo nuevamente" + 
+        "entodos los planetas");
+    }
+}

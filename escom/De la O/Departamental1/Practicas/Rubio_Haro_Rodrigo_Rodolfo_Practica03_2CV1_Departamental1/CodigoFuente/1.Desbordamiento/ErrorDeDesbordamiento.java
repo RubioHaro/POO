3c6cce378c59671ff132f191006af643c5db1f74
Rodrigo Rubio Haro <chavo0022009@gmail.com>
2020-03-02 15:48:20 -0600

    ESCOM: 3.1 Error de desbordamiento was added

---
+++ escom/De la O/Departamental1/Practicas/Rubio_Haro_Rodrigo_Rodolfo_Practica03_2CV1_Departamental1/CodigoFuente/1.Desbordamiento/ErrorDeDesbordamiento.java
@@ -0,0 +1,22 @@
+/*Rodrigo R. Rubio Haro*/
+
+// se corrigió clase por class
+public class ErrorDeDesbordamiento {
+
+	public static void main(String[] args) {
+		ErrorDeDesbordamiento error = new ErrorDeDesbordamiento();
+		error.imprimirError();
+	}
+
+	int positivo = (int) (500000000000000L + 5000000000000L);
+	int negativo = (int) (-2 - 2000000000000L);
+
+	// Se incluyo el metodo imprimirError en la clase ErrorDeDesbordamiento
+	public void imprimirError (){
+
+		// se corrigió la instrucción System.out.println (le faltaban los puntos y tenia argumentos de mas)	
+		System.out.println("positivo contiene: " + positivo);
+		System.out.println("negativo contiene: " + negativo);
+	}
+} //corchete inecesario borrado
+
